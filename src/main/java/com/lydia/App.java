152fe78076c8b47e678778c06a451970a849c915
Lydia <lydia152085@gmail.com>
2026-07-08 16:06:06 +0300

    Initial Java 21 Maven project

---
+++ src/main/java/com/lydia/App.java
@@ -0,0 +1,17 @@
+package com.lydia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println(" Java 21 Maven Demo");
+        System.out.println(" Build Successful!");
+        System.out.println("================================");
+
+    }
+}
---
+++ src/test/java/com/lydia/AppTest.java
@@ -0,0 +1,13 @@
+package com.lydia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
